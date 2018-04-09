d6508b2176b8cfef55830352aa2aef8e8d804659
Alex Rostron <alex.rostron24@gmail.com>
2018-04-09 17:54:39 -0600

    Properly insert the start and endpoints of random maps

--- project-files/mapGenerator.py
+++ project-files/mapGenerator.py
@@ -40,7 +40,7 @@ uint8_t map1[2][16][12] = ((
 
 #endif"""
 
-def createMapFile(w=16,h=12):
+def createMapFileRandom(w=16,h=12):
     """
     randomly generates a map.h file, returns the name of the file (mapXXXX.h)
     """
@@ -50,7 +50,9 @@ def createMapFile(w=16,h=12):
     #100 on 4s
     # 3,4,5,7,6,2,0,1 ten's
 
-    valid_tiles = ['0', '0', '0', '0', '0', '0', '1', '1', '1', '1', '1', '03','13','23','33',\
+    valid_tiles = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', \
+     '1', '1', '1', '1', '1', '1', '1', '1', '1', \
+     '03','13','23','33',\
     '43','53','63','73','04','14','24','34','44','54','64','74','104','114',\
     '124','134','144','154','164','174']
 
@@ -59,17 +61,11 @@ def createMapFile(w=16,h=12):
     for i in range(w*h*2):
         selected_tiles.append(valid_tiles[randint(0,len(valid_tiles)-1)])
 
-    #generate start and end points
-    # start points and end points arent in both graphs...
-    start_spot = randint(0,len(valid_tiles)-1)
-    end_spot = randint(0,len(valid_tiles)-1)
-    while start_spot == end_spot:
-        start_spot = randint(0,len(valid_tiles)-1)
-        end_spot = randint(0,len(valid_tiles)-1)
-
-    # start and end are not the same, add to selected tiles
-    selected_tiles[start_spot] = '6'
-    selected_tiles[end_spot] = '7'
+    #randomly insert the start and enpoints for the graph
+    selected_tiles[randint(0,((w*h)//2))] = '6'
+    selected_tiles[randint(0+((w*h)//2),((w*h)//2)+((w*h)//2))] = '7'
+    selected_tiles[randint(0+((w*h)//2)+((w*h)//2),((w*h)//2)+((w*h)//2)+((w*h)//2))] = '6'
+    selected_tiles[randint(0+((w*h)//2)+((w*h)//2)+((w*h)//2),((w*h)//2)+((w*h)//2)+((w*h)//2)+((w*h)//2))] = '7'
 
     #map finished, substitute in numbers then replace '()' for c++
     mapString = bracketTemplate.format(*selected_tiles)
@@ -82,3 +78,6 @@ def createMapFile(w=16,h=12):
         print("Created file:", title)
 
     return title
+
+if __name__ == '__main__':
+    createMapFileRandom()
