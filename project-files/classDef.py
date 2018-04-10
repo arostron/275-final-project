6a7b6e07b9db99a8bd9a0e001702b24a64d6879d
Alex Rostron <alex.rostron24@gmail.com>
2018-04-09 21:52:04 -0600

    Clean up and comment classDef

--- project-files/classDef.py
+++ project-files/classDef.py
@@ -1,29 +1,36 @@
 class MetaBush:
     def __init__(self,map_array, width, height):
-        # A dictionary of nodes with the tuple names as keys
-        #print("---------------")
+
         self.nodes = set()
+        # translate maps tuple pairs to actual node objects
         self.translate = dict()
-        # A dictionary connect
+        # Connections is a list of all edges in the graph
         self.connect = []
-        #
         self.start = None
         self.end = None
 
+        # dictionary for color conversion
         self.convert = {0:"RED",1:"ORANGE",2:'PINK',3:'WHITE',4:'YELLOW',5:'GREEN',6:'TEAL',7:'BLUE'}
 
-        self.grasses = [] #elements of this dictionary
+        # color based dictionary storage of nodes
+        self.grasses = []
         self.doors = {"RED":[],"ORANGE":[],'PINK':[],'WHITE':[],'YELLOW':[],'GREEN':[],'TEAL':[],'BLUE':[]}
         self.switchs = {"RED":[],"ORANGE":[],'PINK':[],'WHITE':[],'YELLOW':[],'GREEN':[],'TEAL':[],'BLUE':[]}
         self.buttons = {"RED":[],"ORANGE":[],'PINK':[],'WHITE':[],'YELLOW':[],'GREEN':[],'TEAL':[],'BLUE':[]}
-        self.state = {"RED":False,"ORANGE":False,'PINK':False,'WHITE':False,'YELLOW':False,'GREEN':False,'TEAL':False,'BLUE':False}
 
-        #print(map_array)
+        #where the magic happens...
         self.mapToMetaBush(map_array, width, height)
 
     def mapToMetaBush(self, map_array, width, height):
+        """
+        take in an int array repressenting the two maps as well as their individual
+        heights and widths. map_array should be extracted from reader.py
+        """
 
         def grassFlood(x,y,node):
+            """
+            recursive floodfill for grasstiles (and start and end tiles)
+            """
             node.contained_coords.append((x,y))
             if map_array[x][y] == 7:
                 self.start = node.coord
@@ -38,9 +45,10 @@ class MetaBush:
                     grassFlood(pair[0],pair[1],node)
 
 
-        #Grass run
+        #Grass run O(n + m), m is # of grass tiles flood fill
         for i in range(width):
             for j in range(height):
+                # for all values, if grass, floodfill to construct grass node
                 if map_array[i][j] in {0,6,7}:
                     new_grass = Grass(i,j)
                     self.nodes.add((i,j))
@@ -48,7 +56,7 @@ class MetaBush:
                     self.translate[(i,j)] = new_grass
                     grassFlood(i,j,new_grass)
 
-        # Map run
+        # Map run O(n)
         #0grass -- 1wall -- 3switch -- 4door -- 5Pp -- 6end -- 7 Player start
         for i in range(width):
             for j in range(height):
@@ -70,17 +78,20 @@ class MetaBush:
                     self.buttons[self.convert[(val - type)//10]].append((i,j))
                     self.translate[(i,j)] = new
 
-        #Connections run
+        #Connections run O(n)
         for i in range(width):
             for j in range(height):
+                # for all tiles
                 val = map_array[i][j]
                 type = (val % 10)
                 if (type in {0,1,6,7}):
+                    # if grass, door, or start , or finish skip
                     continue
                 surround = [(i+1,j),(i,j+1),(i-1,j),(i,j-1)]
                 for pair in surround:
                     if (pair[0] < 0) or (pair[1] < 0) or (pair[0] >= width) or (pair[1] >= height):
                         continue
+                    # for all valid neighbours of current tile create an edge
                     if map_array[pair[0]][pair[1]] % 10 in {0,6,7}:
                         for item in self.grasses:
                             if (pair[0],pair[1]) in item.contained_coords:
@@ -93,7 +104,7 @@ class MetaBush:
                         self.connect.append(((i,j),(pair[0],pair[1])))
                         self.translate[(i,j)].connect.append((pair[0],pair[1]))
 
-
+#node definitions
 class Node:
     def __init__(self,X,Y,node_type):
         self.connect = list() # Sub nodes
