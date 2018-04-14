2af5ed6bd5ffbf9a0d75bae6ab691f358cf32977
Alex Rostron <alex.rostron24@gmail.com>
2018-04-13 21:38:58 -0600

    Remove extra start file

--- project-files/start.py
+++
@@ -1,9 +0,0 @@
-import reader
-import classDef
-
-
-def setup(filename="Map.h"):
-	map1, map2, w, h = reader.getRawMap(filename)
-	busha = classDef.MetaBush(map1, w, h)
-	bushb = classDef.MetaBush(map2, w, h)
-	return busha, bushb
